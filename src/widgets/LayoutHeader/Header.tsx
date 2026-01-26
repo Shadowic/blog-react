67fca36ad2bb89e751b7f09a9d01b1d746d385e2
shadowic <shadowic.ya@gmail.com>
2026-01-26 23:35:56 +0300

    fix:minor styles

--- src/widgets/LayoutHeader/Header.tsx
+++ src/widgets/LayoutHeader/Header.tsx
@@ -1,23 +1,16 @@
-import { useCallback, useState } from "react";
+import { useState } from "react";
 import type { FC } from "react";
-import { useModal } from "../../shared/lib/context/ModalContext";
 import { ThemeSwitcher } from "../../features/ThemeSwitcher/ui/ThemeSwitcher";
 import { LangSwitcher } from "../../features/LangSwitcher/ui/LangSwitcher";
 import { Navigation } from "../../shared/ui/Navigation/Navigation";
 import { useTranslation } from "react-i18next";
 
-import { Button } from "../../shared/ui/Button";
 import styles from "./Header.module.scss";
 import { Container } from '../../shared/ui/Container/Container';
 
 export const Header: FC = () => {
   const { t } = useTranslation();
-  const { openModal } = useModal();
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-  const handleAboutClick = useCallback(() => {
-    openModal();
-  }, [openModal]);
 
   return (
     <header className={styles.header}>
